1c81d6f92273ced52b8deeb62c60eedbebebffd5
heyadrsh <heyadrsh@gmail.com>
2023-08-01 02:00:51 +0530

    till own corrected numeric-hollow-full-pyramid.cpp

--- 05 Contional Loops & Patterns/Patterns/selfwork/numeric-hollow-full-pyramid.cpp
+++ 05 Contional Loops & Patterns/Patterns/selfwork/numeric-hollow-full-pyramid.cpp
@@ -6,6 +6,7 @@ int main(){
   cout<<"Enter row/s count: ";
   cin>>rowCount;
   int leftSideNumberN=2;
+  int middleLastNumbersline=2;
 
   for(int row=0; row<rowCount; row+=1){
     for(int leftSideSpace=0; leftSideSpace<rowCount-(row+1); leftSideSpace+=1){
@@ -13,30 +14,31 @@ int main(){
     }
     cout<<1<<" ";
     for(int middleSpace=0; middleSpace<(row); middleSpace+=1){
-      if( row!=(rowCount-1)){
-        cout<<"   ";
+      if( row!=(rowCount-1) && row!=1){
+        cout<<"    ";
       }
       else{
+        if(row==1){
+          cout<<"   ";
+        }
+      else{
         if(row==(rowCount-1)){
-          int middleLastNumbersline=2;
-          for(int middleLastNumbers=0; middleLastNumbers<(row); middleLastNumbers+=1){
-            cout<<" "<<middleLastNumbersline<<" ";
+          for(int middleLastNumbers=0; middleLastNumbers<(1); middleLastNumbers+=1){
+            cout<<"  "<<middleLastNumbersline<<"  ";
             middleLastNumbersline+=1;
+      }
           }
         }
       }
-      // int leftSideNumberN=2;
-      // for(int leftSideNumber=0; leftSideNumber<(rowCount-(1-row)); leftSideNumber+=1){
-      //   cout<<" "<<leftSideNumberN<<" ";
-      //   leftSideNumberN+=1;
-      // }
-      if(row==0){
+
+    }
+      if(row==0 || row==rowCount-1){
         cout<<"";
       }
       else{
-        cout<<".";
+        cout<<""<<leftSideNumberN;
+        leftSideNumberN+=1;
       }
-    }
     cout<<endl;
   }
 }
