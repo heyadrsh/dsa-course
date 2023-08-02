17267bce1381e08a085e42e142afe8e23eee8c30
heyadrsh <heyadrsh@gmail.com>
2023-08-02 23:15:36 +0530

    till 06 Week-1 [Connect]

---
+++ Etc codes/typcasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a=5;
+  char b='d';
+  int c=a+b;
+  cout<<c<<endl;
+
+  float d=2.1 + c;
+  cout<<d<<endl;
+
+  float e=2.7;
+  int f=157;
+  int diff=f-e;
+  cout<<diff<<endl;
+
+}
