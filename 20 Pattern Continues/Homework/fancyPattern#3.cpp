f7bff3fe9efb74d9c97d86248932795ac9c7fbe1
heyadrsh <heyadrsh@gmail.com>
2023-08-06 14:36:21 +0530

    till Corrected fancyPattern#3.cpp

--- 20 Pattern Continues/Homework/fancyPattern#3.cpp
+++ 20 Pattern Continues/Homework/fancyPattern#3.cpp
@@ -6,17 +6,30 @@ int main(){
   cout<<"Enter row/s: ";
   cin>>rowCount;
   cout<<endl;
+  int numIncDec=0;
+  int numDecDec=rowCount-2;
 
-  for (int row=0; row<rowCount; row+=1){
+  for (int row=0; row<2*rowCount+1; row+=1){
     cout<<"*";
-    if(row!=0 || row!=rowCount-row){
-      for(int numLeft=1; numLeft<row+1; numLeft+=1){
-        cout<<" "<<numLeft;
+    if(row>=1 && row<rowCount+1){
+      for(int numInc=0; numInc<row; numInc+=1){
+        cout<<" "<<numInc+1;
       }
-      for(int numRight=row-2+1; numRight>0; numRight-=1){
-        cout<<" "<<numRight;
+      for(int numDec=row-1; numDec>0; numDec-=1){
+        cout<<" "<<numDec;
       }
-      cout<<" *";
+      cout<<" "<<"*";
+    }
+    if(row>=rowCount+1){
+      for(int numInc=1; numInc<rowCount-numIncDec; numInc+=1){
+        cout<<" "<<numInc;
+      }
+      numIncDec+=1;
+      for(int numDec=numDecDec; numDec>0; numDec-=1){
+        cout<<" "<<numDec;
+      }
+      numDecDec-=1;
+      cout<<" "<<"*";
     }
     cout<<endl;
   }
