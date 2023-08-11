7b9692517dedf7f0a34a1a65da44e4e089b5c7bb
heyadrsh <heyadrsh@gmail.com>
2023-08-11 23:58:48 +0530

    till Corrected allPrimeN.cpp

--- 22 Functions & some Problem Statements/Homework/allPrimeN.cpp
+++ 22 Functions & some Problem Statements/Homework/allPrimeN.cpp
@@ -1,8 +1,36 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int primeTillN(int n){
-  int num=2;
-  for(int i=0; i<n; i++){
+bool isPrime(int n) {
+  // Corner cases
+  if (n <= 1) {
+    return false;
   }
+
+  // Check if n is divisible by any number from 2 to n-1
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void printPrimes(int n) {
+  for (int i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      cout << i << " ";
+    }
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  printPrimes(n);
+
+  return 0;
 }
