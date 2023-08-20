4b8bd5307a40b971e9ec753c4238a3513aaa74f5
heyadrsh <heyadrsh@gmail.com>
2023-08-20 22:02:31 +0530

    starting of 45 Arrays - Class 3, till 47:51min

---
+++ 45 Arrays - Class 3/2dArrayBasics.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+  //decalaration
+  int arr[56][27];
+
+  //intialization
+  int brr[3][3]={
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}};
+
+    //row-wise print
+  cout<<"Row-wise: "<<endl<<endl;
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      cout<<brr[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+
+
+  cout<<"______________________________"<<endl;
+  cout<<endl;
+    //column-wise print
+  cout<<"column-wise: "<<endl<<endl;
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      cout<<brr[j][i]<<" ";
+    }
+    cout<<endl;
+  }
+} 
