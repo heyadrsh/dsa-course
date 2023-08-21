29069b15f7e8e8080c8b8b62e8c5bdf64f6d91f5
heyadrsh <heyadrsh@gmail.com>
2023-08-21 22:44:59 +0530

    till  45 Arrays - Class 3

---
+++ 45 Arrays - Class 3/rowSum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int arr[5][4]={{1,2,3,4},{2,3,4,1},{5,6,1,3},{2,4,6,8},{1,9,9,6}};
+  for(int i=0; i<5;i++){
+    int rowSum=0;
+    for(int j=0; j<4; j++){
+      rowSum+=arr[i][j];
+    }
+    cout<<rowSum<<" ";
+  }
+}
