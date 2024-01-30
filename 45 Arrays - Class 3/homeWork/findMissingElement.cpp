ed9936a162ddcb9ac50b849fa15ea0aae7a9ca07
heyadrsh <heyadrsh@gmail.com>
2024-01-30 22:50:24 +0530

    till findmissingelements

--- 45 Arrays - Class 3/homeWork/findMissingElement.cpp
+++ 45 Arrays - Class 3/homeWork/findMissingElement.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
- vector<vector<int>>arr(3,vector<int>(3,0));
+ vector<vector<int>>arr(3,vector<int>(3,0));  
 
  for(int i=0; i<arr.size(); i++){
     for(int j=0; j<arr[i].size(); j++){
--- 45 Arrays - Class 3/homeWork/findmissing.py
+++ 45 Arrays - Class 3/homeWork/findmissing.py
@@ -1,11 +1,8 @@
-arr=[22,23,24,25,26,28, 29, 31]
+arr=[22,23,24,25,26,22,28,29, 31]
 temp=arr[0]-1
 
-for i in arr:
-  temp+=1
-  if temp==i:
-    continue
-  if temp!=i:
-    print(temp)
-    temp+=1
-
+for i in range(len(arr)):
+  for j in range(len(arr)):
+    if arr[i]==arr[j]:
+      print(arr[i])
+      break
