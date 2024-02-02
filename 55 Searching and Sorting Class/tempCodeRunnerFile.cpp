e5588a441921839b169e3851df94ec50af66e056
heyadrsh <heyadrsh@gmail.com>
2024-02-02 10:10:04 +0530

    till last occurance

---
+++ 55 Searching and Sorting Class/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+	// while(s<=e){
+	// 	if(v[m]==target){
+	// 		firstIndex=m;
+	// 		e=m-1;
+	// 	}
+	// 	else if(target<v[m]){
+	// 		e=m-1;
+	// 	}
+	// 	else if(target>v[m]){
+	// 		s=m+1;
+	// 	}
+  //   m=(s+(e-s)/2);
+	// }
