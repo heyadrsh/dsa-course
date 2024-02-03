2e46a9399264219a94c5b843f733affca7489227
heyadrsh <heyadrsh@gmail.com>
2024-02-03 23:55:00 +0530

    till incorrect missingElementInAnArrayUsingBinarySearch.cpp

--- 55 Searching and Sorting Class/text.cpp
+++ 55 Searching and Sorting Class/text.cpp
@@ -4,52 +4,35 @@
 using namespace std;
 
 int main(){
-	vector<int>v{1,2,3,3,4,4,4,4,4,4,5,6,7,8};
-	int target=4;
-	int s=0;
-	int e=v.size()-1;
-	int m=(s+(e-s)/2);
-	int firstIndex=-1;
-  int lastIndex=-1;
-  int total=0;
+	vector<int>v{1,2,3,4,5,7,8};
 
-	while(s<=e){
-		if(v[m]==target){
-			firstIndex=m;
-			e=m-1;
-		}
-		else if(target<v[m]){
-			e=m-1;
-		}
-		else if(target>v[m]){
-			s=m+1;
-		}
-    m=(s+(e-s)/2);
-	}
+  int start=0;
+	cout<<"iStart: "<<start<<endl;
+  int end=v.size()-1;
+	cout<<"iend: "<<end<<endl;
+  int mid=(start+end)/2;
+	cout<<"imid: "<<mid<<endl;
 
-  cout<<"First Index:"<<firstIndex<<endl;
-
-	s=0;
-	e=v.size()-1;
-	m=(s+(e-s)/2);
-
-	while(s<=e){
-		if(v[m]==target){
-			lastIndex=m;
-			s=m+1;
-		}
-		else if(target<v[m]){
-			e=m-1;
-		}
-		else if(target>v[m]){
-			s=m+1;
-		}
-    m=(s+(e-s)/2);
-	}
-  cout<<"Last Index:"<<lastIndex<<endl;
-
-  total=lastIndex-firstIndex+1;
-  cout<<"Total Elements:"<<total<<endl;
-
-	return 0;
+  while(start<end){
+    if(v[mid]==v[start]+mid){
+      start=mid+1;
+			cout<<"start: "<<start<<endl;
+			cout<<"mid: "<<mid<<endl;
+			cout<<"end: "<<end<<endl;
+			cout<<"---"<<endl;
+    }
+    else{
+      end=mid-1;
+			cout<<"start: "<<start<<endl;
+			cout<<"mid: "<<mid<<endl;
+			cout<<"end: "<<end<<endl;
+			cout<<"---"<<endl;
+    }
+    mid=start+(end-start)/2;
+			cout<<"start: "<<start<<endl;
+			cout<<"mid: "<<mid<<endl;
+			cout<<"end: "<<end<<endl;
+			cout<<"---"<<endl;
+  }
+	cout<<"Missing Element: "<<mid+2<<endl;
 }
